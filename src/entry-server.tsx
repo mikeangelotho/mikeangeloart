a963a8312020a913eaba95d3e33eaff9450029e6
mikeangelotho <ditomasso.m@gmail.com>
2026-02-08 16:50:52 -0500

    gtag fix

--- src/entry-server.tsx
+++ src/entry-server.tsx
@@ -6,6 +6,15 @@ export default createHandler(() => (
     document={({ assets, children, scripts }) => (
       <html lang="en" class="dark">
         <head>
+          {/* Google tag (gtag.js) */}
+          <script async src="https://www.googletagmanager.com/gtag/js?id=G-WH1W29P097"></script>
+          <script>{`
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-WH1W29P097');`}
+          </script>
           <meta charset="utf-8" />
           <meta name="viewport" content="width=device-width, initial-scale=1" />
           <link rel="icon" href="https://cdn.mikeangelo.art/favicon.svg" />
