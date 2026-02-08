e44a53296b43a32650e55d4ffe49bc1fadb8b779
mikeangelotho <ditomasso.m@gmail.com>
2026-02-08 16:30:31 -0500

    added google tag manager

--- src/app.tsx
+++ src/app.tsx
@@ -5,10 +5,21 @@ import { MetaProvider } from "@solidjs/meta";
 import "./app.css";
 import Navbar from "./layout/Navbar";
 import Footer from "./layout/Footer";
+import { NoHydration } from "solid-js/web";
 
 export default function App() {
   return (
     <MetaProvider>
+      <NoHydration>
+        {/* Google tag (gtag.js) */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-WH1W29P097"></script>
+        <script innerHTML={`window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments)}
+  gtag('js', new Date());
+
+  gtag('config', 'G-WH1W29P097');`}>
+        </script>
+      </NoHydration>
       <Router
         root={(props) => {
           const location = useLocation();
